99e9b8d6c637954810885030b673ad772a3bf153
Sukh767 <sukhranjanjana767@gmail.com>
2024-10-27 05:01:14 +0530

    Add Private route functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,6 +15,8 @@ import App from "./App.jsx";
 import HomeScreen from "./screnns/HomeScreen.jsx";
 import LoginScreen from "./screnns/LoginScreen.jsx";
 import RegisterScreen from "./screnns/RegisterScreen.jsx";
+import ProfileScreen from "./screnns/ProfileScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +24,10 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        {/** Private Route */}
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
