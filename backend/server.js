606191e6ee9248acbce06abc6e2ad7662d63c78c
Sukh767 <sukhranjanjana767@gmail.com>
2024-10-27 05:49:12 +0530

    Prepare for production

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,41 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import cookieParser from 'cookie-parser'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 connectDB();
 
-const port  = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/api/users',userRoutes)
+app.use("/api/users", userRoutes);
 
-app.get('/',(req,res)=> res.send('Server is working'))
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist"))); //make dist folder static
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname), "frontend", "dist", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is working"));
+}
 
 //use middlware for error handling
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port,()=>{
-    console.log(`Server is started on port ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server is started on port ${port}`);
+});
